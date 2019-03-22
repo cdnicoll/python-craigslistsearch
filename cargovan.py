78530b2bef7ed73edb780afa2dd2fbe003f88a92
cdnicoll <cdnicoll@gmail.com>
2019-03-22 14:45:02 -0700

    modified fetch

--- cargovan.py
+++ cargovan.py
@@ -1,39 +1,40 @@
 from craigslist import CraigslistForSale
 
 filters = {
-    'query': {'cargo van', 'chevy express', 'Savana', 'Sprinter'},
     'search_titles':True,
     'min_price': 5000,
     'max_price': 11000
 }
 
-cl_h = CraigslistForSale(
-    site='vancouver', 
-    area={'van', 'nvm', 'bnc', 'rds', 'pml', 'rch'}, 
-    category='cta',
-    filters=filters
-)
+def get_result(query):
+    filters['query'] = query
 
-for result in cl_h.get_results(sort_by='newest', limit=10):
-    print result['name']
-    print result['url']
-    print result['price']
-    print result['datetime']
-    print '\n'
+    cl_h = CraigslistForSale(
+        site='vancouver', 
+        area={'van', 'nvm', 'bnc', 'rds', 'pml', 'rch'}, 
+        category='cta',
+        filters=filters
+    )
+    
+    for result in cl_h.get_results(sort_by='newest', limit=10):
+        print result['name']
+        print result['url']
+        print result['price']
+        print result['datetime']
+        print '\n'
 
+print 'Fetching ----> Cargo Van'
+get_result('cargo van')
+print '\n\n'
 
+print 'Fetching ----> Chevy Express'
+get_result('chevy express')
+print '\n\n'
 
+print 'Fetching ----> savana'
+get_result('savana')
+print '\n\n'
 
-
-# {'name': u'GMC Savana/Chevy Express Benches',
-#  'repost_of': None,
-#  'has_image': True,
-#  'url': u'https://vancouver.craigslist.org/bnc/pts/d/burnaby-gmc-savana-chevy-express-benches/6830377298.html',
-#  'has_map': True,
-#  'price': u'$250',
-#  'geotag': (49.210858,
-#  -122.955551),
-#  'where': u'Burnaby',
-#  'id': u'6830377298',
-#  'datetime': u'2019-03-21 16:34'
-#  }
+print 'Fetching ----> sprinter'
+get_result('sprinter')
+print '\n\n'
