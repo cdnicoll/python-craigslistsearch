09d7daec82a5684d575cc1a9c5361466e62cc45d
cdnicoll <cdnicoll@gmail.com>
2019-03-22 11:25:20 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# python-craigslist
+
+A Tool to search craigslist using [python-craigslist](https://github.com/juliomalegria/python-craigslist)
+
+## To Install
+Run: `pip install python-craigslist`
+
+## Useage:
+`python ./cargovan.py`
---
+++ cargovan.py
@@ -0,0 +1,35 @@
+from craigslist import CraigslistForSale
+
+filters = {
+    'query': 'chevy express', 
+    'search_titles':True,
+    'min_price': 5000,
+    'max_price': 10000
+}
+
+cl_h = CraigslistForSale(site='vancouver', area={'van', 'nvm', 'bnc', 'rds', 'pml', 'rch'}, category='cta',
+                         filters=filters)
+
+for result in cl_h.get_results(sort_by='newest', geotagged=True, limit=5):
+    print result['name']
+    print result['url']
+    print result['price']
+    print result['datetime']
+    print '\n'
+
+
+
+
+
+# {'name': u'GMC Savana/Chevy Express Benches',
+#  'repost_of': None,
+#  'has_image': True,
+#  'url': u'https://vancouver.craigslist.org/bnc/pts/d/burnaby-gmc-savana-chevy-express-benches/6830377298.html',
+#  'has_map': True,
+#  'price': u'$250',
+#  'geotag': (49.210858,
+#  -122.955551),
+#  'where': u'Burnaby',
+#  'id': u'6830377298',
+#  'datetime': u'2019-03-21 16:34'
+#  }
