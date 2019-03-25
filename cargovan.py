121c2b036538cb28452daac639908c6055e1656f
Cody Nicoll <cdnicoll@gmail.com>
2019-03-25 08:50:35 -0700

    Updates

--- cargovan.py
+++ cargovan.py
@@ -6,6 +6,12 @@ filters = {
     'max_price': 11000
 }
 
+# filters = {
+#     'search_titles':True,
+#     'min_price': 11000,
+#     'max_price': 17000
+# }
+
 def get_result(query):
     filters['query'] = query
 
@@ -29,9 +35,14 @@ def fetching(type):
     get_result(type)
     print '\n\n'
 
-
 fetching('cargo van')
 fetching('chevy express')
 fetching('e250')
 fetching('savana')
 fetching('sprinter')
+
+print 'Dont forget to checkout these sites:'
+print 'https://www.autotrader.ca/heavy-trucks/cube-step-cargo%20vans/bc/?srt=3&pRng=%2C11000&oRng=%2C190000&prv=British%20Columbia&loc=v3m0c5&sts=Used'
+print '------------------------'
+print 'https://ca.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?sourceContext=untrackedExternal_false_0&newSearchFromOverviewPage=true&inventorySearchWidgetType=BODYSTYLE&bodyTypeGroup=bg8&zip=V6J&distance=100&startYear=2005&endYear=&minPrice=0&maxPrice=12000&maxMileage=190000&showNegotiable=false&modelChanged=undefined&filtersModified=true'
+print '\n'
